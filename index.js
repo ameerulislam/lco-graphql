1e73aa0136cd55c3f493f6d23a41b7602b8d1581
Ameer Ul Islam <ameerulislam@gmail.com>
2022-02-16 21:14:13 +0600

    building and learing graphql

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Start your es6 scripts here
+import express from 'express'
+import resolvers from './resolvers'
+import schema from './schema'
+
+import {graphqlHTTP} from 'express-graphql'
+
+
+const app = express()
+
+    app.get("/", (req, res) => {
+        res.send("up and running ameer")
+    })
+
+const root = resolvers;//{lco: () => console.log('Learn graqphql ')}
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}))
+
+app.listen(8082, () => console.log("Running at http://localhost:8082"))
---
+++ resolvers.js
@@ -0,0 +1,32 @@
+import {nanoid} from 'nanoid'
+
+class Course {
+    constructor(id, {
+        courseName, category, price, langauge, email, stack, teachingAssists
+    }){
+        this. id = id
+        this.courseName = courseName
+        this.category = category
+        this.price = price
+        this.langauge = langauge
+        this.email = email
+        this.stack = stack
+        this.teachingAssists = teachingAssists
+
+    }
+}
+
+const courseholder = {}
+
+const resolvers = {
+    getCourse : ({id}) => {
+        return new Course(id, courseholder[id])
+    }, 
+    createCourse : ({input }) => {
+        let id = nanoid()
+        courseholder[id] = input
+        return new Course(id, input)
+    }
+}
+
+export default resolvers;
